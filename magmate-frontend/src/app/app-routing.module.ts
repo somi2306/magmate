19a367f994ad4bb2262967ad49c63258fb9e0003
ayakandoussi <ayakandoussi1905@gmail.com>
2025-05-28 11:47:17 +0100

    event

--- magmate-frontend/src/app/app-routing.module.ts
+++ magmate-frontend/src/app/app-routing.module.ts
@@ -70,7 +70,7 @@ const routes: Routes = [
     canActivate: [AuthGuard],
   },
 
-  { path: 'events/:id', component: EventsDetailsComponent },
+  { path: 'events/:id', component: EventsDetailsComponent, canActivate: [AuthGuard] },
 ];
 
 @NgModule({
